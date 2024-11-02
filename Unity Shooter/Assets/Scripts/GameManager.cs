cb43641f7bc727c8e1333dcc1aac8a3b601a68df
Ziobots <zionbots@gmail.com>
2024-11-01 21:33:40 -0400

    game working

--- Unity Shooter/Assets/Scripts/GameManager.cs
+++ Unity Shooter/Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ public class GameManager : MonoBehaviour {
 	public GameObject player;
 	public GameObject enemy;
 
-// Start is called before the first frame update
+	// Start is called before the first frame update
 	private void Start() {
 		Instantiate(player, transform.position, Quaternion.identity);
 		InvokeRepeating(nameof(CreateEnemy), 1f, 3f);
