b7025fcf8c5a8c5019fb333ca704fef54485d1b8
Ziobots <zionbots@gmail.com>
2024-11-08 14:28:45 -0500

    fix

--- Unity Shooter/Assets/Scripts/Entity.cs
+++ Unity Shooter/Assets/Scripts/Entity.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour {
     public int lives = 3;
+    private bool _died = false;
 
     public GameObject explosionPrefab;
 
     public virtual void TakeDamage(int damage) {
         lives -= damage;
+        
         if (lives <= 0) {
             Kill();
         }
     }
 
     protected virtual void Kill() {
+        if (_died) return;
+        _died = true;
+        
         Instantiate(explosionPrefab, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
